6bc2913d6a66e09a9f84eb411fb709042e971841
AntonVartabedyan <108354956+AntonVartabedyan@users.noreply.github.com>
2022-11-09 16:42:42 +0200

    Delete src directory

--- src/SoundManager.ts
+++
@@ -1,8 +0,0 @@
-import * as PIXI from "pixi.js";
-export class SoundManager {
-    public static audio = new Audio("./assets/audio/bgMusic.mp3");
-    public static audio2 = new Audio("./assets/audio/bgMusic2.mp3");
-    public static brickSound = new Audio("./assets/audio/brickHit.wav");
-    public static paddleHitSound = new Audio("./assets/audio/paddleHit.wav");
-    public static WallHitSound = new Audio("./assets/audio/wallHit.wav");
-}
